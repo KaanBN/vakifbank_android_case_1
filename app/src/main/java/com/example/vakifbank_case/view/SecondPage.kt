58938aad3f3745dfbaecdf0dbce7763856a028eb
KaanBN <kaan.mlnym@hotmail.com>
2023-07-14 17:14:09 +0300

    second page connected with viewmodel

--- app/src/main/java/com/example/vakifbank_case/view/SecondPage.kt
+++ app/src/main/java/com/example/vakifbank_case/view/SecondPage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.vakifbank_case.databinding.ActivitySecondPage2Binding
 import com.example.vakifbank_case.viewmodel.SecondPageViewModel
@@ -18,14 +19,15 @@ class SecondPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondPage2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        secondVievModel = SecondPageViewModel()
-        secondVievModel.getWeather(33.33,33.33, "hourly")
-        val asd = secondVievModel.observeWeatherLiveData();
-        asd.observe(
-            this
-        ) {
+        secondVievModel = ViewModelProvider(this).get(SecondPageViewModel::class.java)
+        secondVievModel.init(application)
+        secondVievModel.getWeather(33.33,33.33,"hourly")
+
+        secondVievModel.observeWeatherLiveData().observe(this, Observer(){
             println("it: $it")
         }
+
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/vakifbank_case/viewmodel/SecondPageViewModel.kt
+++ app/src/main/java/com/example/vakifbank_case/viewmodel/SecondPageViewModel.kt
@@ -1,18 +1,32 @@
 package com.example.vakifbank_case.viewmodel
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vakifbank_case.model.WeatherResponse
-import com.example.vakifbank_case.repository.SecondPageRepository
 import com.example.vakifbank_case.retrofit.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class SecondPageViewModel : ViewModel() {
+
+class SecondPageViewModel() : ViewModel() {
     private var weatherLiveData = MutableLiveData<WeatherResponse>()
-
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val _currentLocation = MutableLiveData<Location>()
+    val currentLocation : LiveData<Location> = _currentLocation
+    fun init(application: Application){
+        LocationServices.getFusedLocationProviderClient(application)
+    }
     fun getWeather(lat : Double, lon: Double, exclude: String){
         RetrofitClient.apiInterface.getWeather(lat, lon, exclude).enqueue(
             object : Callback<WeatherResponse>{
@@ -32,6 +46,15 @@ class SecondPageViewModel : ViewModel() {
             }
         )
 
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLocation(){
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener {
+                location: Location? ->
+                _currentLocation.value = location
+            }
     }
 
     fun observeWeatherLiveData() : LiveData<WeatherResponse> {
