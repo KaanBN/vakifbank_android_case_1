a4b2eeef17c0c0f5884c1b8a2cefe0404215d421
KaanBN <kaan.mlnym@hotmail.com>
2023-07-14 11:54:13 +0300

    first page has changed

--- app/src/main/java/com/example/vakifbank_case/view/MainActivity.kt
+++ app/src/main/java/com/example/vakifbank_case/view/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.vakifbank_case
+package com.example.vakifbank_case.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,9 @@ import com.example.vakifbank_case.ui.theme.Vakifbank_caseTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun buttonClick(view: View) {
 
     }
 }
