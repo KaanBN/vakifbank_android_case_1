dc37b0f7a41024021921ee26c3c1b68adbe047a6
KaanBN <kaan.mlnym@hotmail.com>
2023-07-17 09:29:03 +0300

    location detection added

--- app/src/main/java/com/example/vakifbank_case/viewmodel/SecondPageViewModel.kt
+++ app/src/main/java/com/example/vakifbank_case/viewmodel/SecondPageViewModel.kt
@@ -5,18 +5,24 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.pm.PackageManager
 import android.location.Location
-import android.location.LocationManager
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vakifbank_case.model.WeatherResponse
 import com.example.vakifbank_case.retrofit.RetrofitClient
+import com.example.vakifbank_case.view.SecondPage
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.squareup.picasso.Picasso
 
 
 class SecondPageViewModel() : ViewModel() {
@@ -48,16 +54,44 @@ class SecondPageViewModel() : ViewModel() {
 
     }
 
-    @SuppressLint("MissingPermission")
-    fun getLocation(){
-        fusedLocationProviderClient.lastLocation
-            .addOnSuccessListener {
-                location: Location? ->
+    fun getLocation(application: SecondPage){
+        if (ActivityCompat.checkSelfPermission(
+                application,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                application,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                application,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                101
+            )
+            return
+        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(application)
+        fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+            override fun isCancellationRequested() = false
+        }).addOnSuccessListener { location : android.location.Location? ->
+            if (location != null) {
                 _currentLocation.value = location
             }
+        }
+    }
+
+    fun observeCurrentLocationLiveData() : LiveData<Location> {
+        return currentLocation
     }
 
     fun observeWeatherLiveData() : LiveData<WeatherResponse> {
         return weatherLiveData;
+    }
+
+    fun loadImage(ImageView: ImageView, image: String){
+        val url = "https://openweathermap.org/img/wn/$image.png"
+        Picasso.get().load(url).into(ImageView)
     }
 }
