256252158a3db763829e8b9f5f1a5d3b0879d886
KaanBN <kaan.mlnym@hotmail.com>
2023-07-17 09:29:57 +0300

    deep link added

--- app/src/main/java/com/example/vakifbank_case/view/MainActivity.kt
+++ app/src/main/java/com/example/vakifbank_case/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vakifbank_case.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.example.vakifbank_case.R
 
@@ -13,9 +14,20 @@ class MainActivity : ComponentActivity() {
     }
 
     fun buttonClick(view: View) {
-        val intent = Intent(this@MainActivity, SecondPage::class.java)
-        startActivity(
-            intent
-        )
+        val editText = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.editTextText2) as EditText
+        val apikey = editText.text.toString()
+        if (apikey != "") {
+            val intent = Intent(this@MainActivity, SecondPage::class.java)
+            intent.putExtra("apikey", apikey)
+            startActivity(
+                intent
+            )
+        }
+        else{
+            val intent = Intent(this@MainActivity, SecondPage::class.java)
+            startActivity(
+                intent
+            )
+        }
     }
 }
